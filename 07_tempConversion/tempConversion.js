4a756874e32073c6680d6eb19cd56a4c096b7040
Chris-Wilco <christopher.wilcoski@gmail.com>
2023-12-06 15:36:38 -0800

    update and complete exercises 1-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTemp) {
+  return roundToOne(5 / 9 * (inputTemp - 32));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputTemp) {
+  return roundToOne((inputTemp * 9 / 5) + 32);
 };
+
+function roundToOne(num){
+  return +(Math.round(num + "e+1") + "e-1");
+}
 
 // Do not edit below this line
 module.exports = {
